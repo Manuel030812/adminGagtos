97e3bdff69980e5d3da93b86832b74d8cf065acc
Manuel Lopez <aqnuelmagamer8@gmail.com>
2025-07-09 11:34:10 -0600

    agregando cosas de css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
+import Filter from './components/Filter.vue'
 
 </script>
 
 <template>
   <div>
+    <header>
+      <h1>Planificador de gastos</h1>
+    </header>
+
+    <Filter/>
    
   </div>
   
 </template>
 
-<style scoped>
+<style>
+  
 
 </style>

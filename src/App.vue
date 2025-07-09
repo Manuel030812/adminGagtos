eed312f828a409fe4d100ef77cbc1dd2bef0417f
Manuel Lopez <aqnuelmagamer8@gmail.com>
2025-07-09 12:31:19 -0600

    formulario con un poco de css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Filter from './components/Filter.vue'
+import Presupuesto from './components/Presupuesto.vue'
 
 </script>
 
@@ -7,54 +8,84 @@ import Filter from './components/Filter.vue'
   <div>
     <header>
       <h1>Planificador de gastos</h1>
+      <div class="contenerdor-heder contenedor sombra">
+        <Presupuesto />
+
+      </div>
+
     </header>
 
-    <Filter/>
-   
+    <Filter />
+
   </div>
-  
+
 </template>
 
 <style>
-  :root{
+:root {
   --azul: #3b82f6;
   --blanco: #fff;
   --gris-claro: #f5f5f5;
   --gris: #94a3b4;
   --gris-oscuro: #64748b;
   --negro: #000;
-  }
+}
 
-  html{
-    font-size: 62.5%;
-    box-sizing: border-box;
-  }
-  *,
-  *:before,
-  *:after{
-    box-sizing: inherit;
-  }
-  body{
-    font-size: 1.6rem;
-    font-family: "Lato", sans-serif;
-    background-color: var(--gris-claro);
-  }
-  h1{
-    font-size: 4rem;
-  }
-  h2{
-    font-size: 3rem;
-  }
-  header{
-    background-color: var(--azul);
-  }
-  header h1 {
-    padding: 3rem 0;
-    margin: 0;
-    color: var(--blanco);
-    text-align: center;
-  }
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
 
-  
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
 
+body {
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--gris-claro);
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+
+header {
+  background-color: var(--azul);
+}
+
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--blanco);
+  text-align: center;
+}
+
+.contenedor {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+
+}
+
+.contenerdor-heder {
+  margin-top: -5rem;
+  transform:translateY(5rem);
+  padding: 5rem;
+
+}
+
+.sombra {
+  box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+  background-color: var(--blanco);
+  border-radius: 1.2rem;
+  padding: 5rem;
+
+}
 </style>
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,18 @@
+<template>
+    <form action="">
+        <div class="campo">
+            <label for="">Definir presupuesto</label>
+            <input id="nuevoPresupuesto"
+            class="nuevoPresupuesto" type="number" placeholder="Añade tu presupuesto" />
+        </div>
+        <input  class="submit" value="Definir presupuesto" />
+    </form>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
